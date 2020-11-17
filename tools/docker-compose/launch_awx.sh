731d17fddfdbb71855fdb721197ce79fe2907394
Shane McDonald <shanemcd@redhat.com>
2020-11-17 13:40:45 -0500

    Set SDB_NOTIFY_HOST for all processes

--- tools/docker-compose/launch_awx.sh
+++ tools/docker-compose/launch_awx.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set +x
 
+if [[ "$OS" ==  *"Docker Desktop"* ]]; then
+    export SDB_NOTIFY_HOST='docker.for.mac.host.internal'
+else
+    export SDB_NOTIFY_HOST=$(ip route | head -n1 | awk '{print $3}')
+fi
+
 bootstrap_development.sh
 
 cd /awx_devel
